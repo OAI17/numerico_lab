0ae228fb0c886c63bdcc07eb3bf7aa8e02ad2900
Tomas's Surface <tomasmarmay21@gmail.com>
2022-06-03 14:23:03 -0300

    lab4 done

--- lab4/ej4.py
+++ lab4/ej4.py
@@ -5,20 +5,18 @@ data = np.loadtxt("https://raw.githubusercontent.com/lbiedma/anfamaf2022/main/da
 
 m = len(data)
 
-
 hx = np.arange(1,m+1)
 hy = data
-
 x = 0
 y = 0
 x_cuad = 0
 x_y = 0
 
 for i in range(0,m):
-    x = x + float(np.log(hx[i]))
+    x = x + float((hx[i]))
     y = y + float(np.log(hy[i]))
     x_cuad = x_cuad + (float(hx[i])**2)
-    x_y = x_y + float((hx[i])*float((hy[i])))
+    x_y = x_y + float((hx[i])*float(np.log(hy[i])))
 
 # bx + a
 a = np.exp(((x_cuad*y)-(x_y*x))/((m*x_cuad)-(x**2)))
@@ -29,10 +27,10 @@ hy_new = []
 
 
 for k in range(0,m):
-    fun = (b*hx[k]) + a
+    fun = (a*np.exp((b * hx[k])))
     hy_new.append(fun)
 
 plt.plot(hy_new,label="aprox",color="r")
-plt.scatter(hx,data,label="datos",color="g")
+plt.scatter(hx,hy,label="datos",color="g")
 plt.legend()
 plt.show()
